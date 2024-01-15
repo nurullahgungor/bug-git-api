0109d8d556bdba6039441db044cee25c51e1089b
Nurullah GUNGOR <ngngrx1@gmail.com>
2024-01-15 15:53:02 +0300

    refactor : change test.js file name to server.js file, removed test link branch

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const config = require("./bundle/config");
+const gitRepos = require("./routes/gitRepos");
+
+console.log(config);
+
+
+// Important for project branch
+// Change `/gitRepos` with your link branch.
+app.use("/gitRepos", gitRepos); 
+
+app.listen(config.configTree.port, () => {
+
+    //Default port is 3000, if you want to change this port : Config => config.json => { "port" : "yourPortNumber" }
+    console.log(`Server on ${config.configTree.port}`)
+})
